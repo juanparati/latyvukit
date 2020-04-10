0ad095a8171e1c1940123e4ea1a0fb42927b660c
Juan Lago <juanparati@gmail.com>
2020-04-10 15:43:59 +0200

    Improved bootstrap.ts

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -1,8 +1,8 @@
 /**
  * Load UIkit
  */
-(<any> window).UIkit = require('uikit');
-(<any> window).UIkit.use(require("uikit/dist/js/uikit-icons"));
+window['UIkit'] = require('uikit');
+window['UIkit'].use(require("uikit/dist/js/uikit-icons"));
 
 
 /**
@@ -27,39 +27,63 @@
  * Load and initialize Vue with InertiaJS
  */
 import Vue from 'vue';
+
+
+// Vue composition
+// ===============
 import Composition from '@vue/composition-api';
-import { InertiaApp } from '@inertiajs/inertia-vue';
+
+// It provides compatibility with the new Vue 3.x composition library
+Vue.use(Composition);
+
+import Component from 'vue-class-component';
+
+// Register additional component hooks
+Component.registerHooks([
+    'setup'
+]);
+
+
+// Initialize Axios
+// ================
 import VueAxios from "vue-axios";
 import Axios from "axios";
 
-
-// Libraries to inject
-// ===================
-
-// @ts-ignore
-Vue.use(InertiaApp);
-Vue.use(Composition);
+// InertiaJS use also axios, so it's important initialize Axios before than InertiaJS is initialized
+// in order to avoid some conflicts.
 Vue.use(VueAxios, Axios);
-
-
-
-// Axios tweak
-// ===========
 
 // Set 'XMLHttpRequest' as requested with so the backend can distinguish between
 // normal an AJAX request.
 Vue.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+// User CSRF
+Vue.axios.defaults.withCredentials = true;
+
+
+// Inject InertiaJS
+// ================
+import { InertiaApp } from '@inertiajs/inertia-vue';
+
+// @ts-ignore
+Vue.use(InertiaApp);
+
 
 // Prototype mixins
 // ================
 
-// Inject route helper.
+// Inject route and UIKit helpers.
 //
 // @see https://github.com/tightenco/ziggy
 // @see tsconfig.json
 // @see route-shims.d.ts
-Vue.mixin({ methods: { route: window['route'] } });
+Vue.mixin({
+    methods: {
+        route: window['route'],
+        uikit: window['UIkit'],
+    }
+});
+
 
 
 // Initialize App
